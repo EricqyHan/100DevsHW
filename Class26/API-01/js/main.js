e9443c9e4439af77c192f04130e7c86141245145
EricQyhan <Ericqyhan@gmail.com>
2022-04-08 22:36:11 -0400

    updated class 26 hw files

--- Class26/API-01/js/main.js
+++ Class26/API-01/js/main.js
@@ -1,16 +1,16 @@
-//Example fetch using pokemonapi.co
-fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-  .then((res) => res.json()) // parse response as JSON
-  .then((data) => {
-    console.log(data);
-    //  document.querySelector("img").src = [0];
-  })
-  .catch((err) => {
-    console.log(`error ${err}`);
-  });
+document.querySelector("button").addEventListener("click", getExcuse);
 
-// document.querySelector("button").addEventListener("click", getBird);
+function getExcuse() {
+  let category = document.querySelector("#excuses").value;
 
-// function getBird() {
-
-// }
+  fetch(`https://excuser.herokuapp.com/v1/excuse/${category}`)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data[0]);
+      console.log(data[0].excuse);
+      document.querySelector("span").innerText = data[0].excuse;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
--- Class26/api-template/js/main.js
+++ Class26/api-template/js/main.js
@@ -1,34 +1,16 @@
 //Example fetch using pokemonapi.co
-// document.querySelector("button").addEventListener("click", getExcuse);
+fetch("www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+  .then((res) => res.json()) // parse response as JSON
+  .then((data) => {
+    console.log(data);
+    //  document.querySelector("img").src = [0];
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
 
-// function getExcuse() {
-//   let category = document.querySelector("input").value;
+// document.querySelector("button").addEventListener("click", getBird);
 
-//   fetch(`https://excuser.herokuapp.com/v1/excuse/${category}`)
-//     .then((res) => res.json()) // parse response as JSON
-//     .then((data) => {
-//       console.log(data[0]);
-//       console.log(data[0].excuse);
-//       document.querySelector("h3").innerText = data[0].excuse;
-//     })
-//     .catch((err) => {
-//       console.log(`error ${err}`);
-//     });
+// function getBird() {
+
 // }
-
-document.querySelector("button").addEventListener("click", getExcuse);
-
-function getExcuse() {
-  let category = document.querySelector("#excuses").value;
-
-  fetch(`https://excuser.herokuapp.com/v1/excuse/${category}`)
-    .then((res) => res.json()) // parse response as JSON
-    .then((data) => {
-      console.log(data[0]);
-      console.log(data[0].excuse);
-      document.querySelector("span").innerText = data[0].excuse;
-    })
-    .catch((err) => {
-      console.log(`error ${err}`);
-    });
-}
